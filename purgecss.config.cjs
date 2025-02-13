e655d3db4d498fad9e97113fdef9b35aa23b231c
mohammeddl <mohammed_daali2@um5.ac.ma>
2025-02-13 09:45:38 +0100

    Add package.json files and .npmignore for various modules; configure module types

---
+++ purgecss.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+    content: ["./*.html", "./js/**/*.js"], // Paths to your HTML and JS files
+    css: ["./css/*.css"], // Paths to your CSS files
+    output: "./cleaned-css/", // Output directory for cleaned CSS
+  };
+  
